735948263b4f9256866f353b8222725e377cacc4
foxliveray <275239328@qq.com>
2017-06-07 15:23:34 +0800

    UserDaoImpl修改

--- Contract Management System/contract/src/dao/impl/UserDaoImpl.java
+++ Contract Management System/contract/src/dao/impl/UserDaoImpl.java
@@ -4,233 +4,83 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 import dao.UserDao;
 import model.User;
 import util.AppException;
-import util.DBUtil;
+import util.JDBCUtil;
 
 /**
- * User data access layer implementation class
+ * @author 钱洋
+ * @date 2017年6月7日 上午9:50:27
  */
-public class UserDaoImpl implements UserDao{
+public class UserDaoImpl implements UserDao {
 
 	/**
-	 *  Verify whether exist user that has the same name
-	 * 
-	 * @param name User name
-	 * @return  Return true if there are users with same name,otherwise return false 
+	 * 区分是否有同名的用户存在
+	 * @param name(用户名)
+	 * @return 返回True如果有用户有相同的名字，否则返回false
 	 * @throws AppException
 	 */
+	@Override
 	public boolean isExist(String name) throws AppException {
-		boolean flag = false;// Operation flag
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 		
-		Connection conn = null; // Define database connection object
-		PreparedStatement psmt = null;// Define PreparedStatement object
-		ResultSet rs = null;// Define ResultSet object
-
-		try {
-			conn = DBUtil.getConnection();// Create database connection
-			// Declare operation statement, query records based on user name, "?" is a placeholder
-			String sql = "select id from t_user where name = ? and del = 0";
-
-			psmt = conn.prepareStatement(sql);//  Pre-compiled sql
-			psmt.setString(1, name);// Set values for the placeholder 
-
-			rs = psmt.executeQuery();// Execute the query, return the query results
-			if (rs.next()) {// Determine whether there are values in results set
+		boolean flag = false;
+		try{
+			conn = JDBCUtil.getConnection();
+			String sql = "Select id from t_user where name = ? and del = 0";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, name);
+			rs = pstmt.executeQuery();
+			if (rs.next()){
 				flag = true;
 			}
-		} catch (SQLException e) {
+		} catch (SQLException e){
 			e.printStackTrace();
 			throw new AppException("dao.impl.UserDaoImpl.isExist");
 		} finally {
-			DBUtil.closeResultSet(rs);// Close database query result set
-			DBUtil.closeStatement(psmt);//  Close database object pretreatment
-			DBUtil.closeConnection(conn);// Close database connection object
+			JDBCUtil.closeResultSet(rs);
+			JDBCUtil.ClosePreparedStatement(pstmt);
+			JDBCUtil.closeConnection(conn);
 		}
-		
 		return flag;
 	}
 
 	/**
-	 * Save user information
-	 * 
-	 * @param User object
-	 * @return Return true if saved successfully,otherwise return false
+	 * 保存用户信息
+	 * @param user (user对象实例)
+	 * @return 返回True如果有添加新用户成功，否则返回false
 	 * @throws AppException
 	 */
+	@Override
 	public boolean add(User user) throws AppException {
-		boolean flag = false;// Operation flag
+		Connection conn = null;
+		PreparedStatement pstmt = null;
 		
-		Connection conn = null; // Define database connection object
-		PreparedStatement psmt = null;// Define PreparedStatement object
-		
+		boolean flag = false;
 		int result = -1;
-		try {
-			conn = DBUtil.getConnection();// Create database connection
-			// Declare operation statement,save user information into database, "?" is a placeholder
-			String sql = "insert into t_user (name,password)"
-					+ " values (?,?)";
+		try{
+			conn = JDBCUtil.getConnection();
+			String sql = "Insert into t_user (name,password) values (?,?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, user.getName());
+			pstmt.setString(2, user.getPassword());
 			
-			psmt = conn.prepareStatement(sql);// Pre-compiled sql
-			// Set values for the placeholder 
-			psmt.setString(1, user.getName());
-			psmt.setString(2, user.getPassword());
-
-			result = psmt.executeUpdate();// Execute the update operation,return the affected rows
-			if (result > 0) {
+			result = pstmt.executeUpdate();
+			if (result > 0){
 				flag = true;
 			}
-		} catch (SQLException e) {
+		} catch (SQLException e){
 			e.printStackTrace();
-			throw new AppException("dao.impl.UserDaoImpl.add");
+			throw new AppException("doa.impl.UserDaoImpl.add");
 		} finally {
-			DBUtil.closeStatement(psmt);//  Close database object pretreatment
-			DBUtil.closeConnection(conn);// Close database connection object
+			JDBCUtil.CloseStatement(pstmt);
+			JDBCUtil.closeConnection(conn);
 		}
-		
 		return flag;
-	}
-
-	/**
-	 * Query user number according to the user name and password
-	 * 
-	 * @param name 
-	 * @param password 
-	 * @return User number
-	 * @throws AppException
-	 */
-	public int login(String name, String password) throws AppException {
-		int userId = -1; // Initialize userId
-		
-		//Declare database connection object, pre-compiled object and result set object
-		Connection conn = null;
-		PreparedStatement psmt = null;
-		ResultSet rs = null;
-
-		try {
-			// Create database connection
-			conn = DBUtil.getConnection();
-			// Declare operation statement:query user id according to the user name and password , "?" is a placeholder
-			String sql = "select id from t_user where name = ? and password = ? and del = 0";
-			//  pre-compiled sql
-			psmt = conn.prepareStatement(sql);
-			// Set values for the placeholder
-			psmt.setString(1, name);
-			psmt.setString(2, password);
-			// Execute the query operation
-			rs = psmt.executeQuery();
-			// Query record and get  user id
-			if (rs.next()) {
-				userId = rs.getInt("id");
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new AppException("dao.impl.UserDaoImpl.login");
-		} finally {
-			// Close database object operation, release resources
-			DBUtil.closeResultSet(rs);
-			DBUtil.closeStatement(psmt);
-			DBUtil.closeConnection(conn);
-		}
-		
-		return userId;
-	}
-
-	/**
-	 * Query user information according to id
-	 * 
-	 * @param id User id
-	 * @return User User object
-	 * @throws AppException
-	 */
-	public User getById(int id) throws AppException {
-		// Declare user object
-		User user = null;
-		
-		//Declare database connection object, pre-compiled object and result set object
-		Connection conn = null;
-		PreparedStatement psmt = null;
-		ResultSet rs = null;
-		
-		try {
-			// Create database connection
-			conn = DBUtil.getConnection();
-			// Declare operation statement:query user information according to the user id , "?" is a placeholder
-			String sql = "select id,name,password "
-					+"from t_user "
-					+"where id = ? and del = 0";
-			// pre-compiled sql
-			psmt = conn.prepareStatement(sql);
-			// Set values for the placeholder
-			psmt.setInt(1, id);
-			// Query resultSet
-			rs = psmt.executeQuery();
-			
-			// Save user information in Pole entity object when queried out resultSet
-			if (rs.next()) {
-				user = new User(); // Instantiate user objects
-				// Set value to user object
-				user.setId(rs.getInt("id"));
-				user.setName(rs.getString("name"));
-				user.setPassword(rs.getString("password"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new AppException("dao.impl.UserDaoImpl.getById");
-		} finally {
-			// Close database object operation, release resources
-			DBUtil.closeResultSet(rs);
-			DBUtil.closeStatement(psmt);
-			DBUtil.closeConnection(conn);
-		}
-		
-		return user;
-	}
-
-	/**
-	 * Query user id set
-	 * 
-	 * @return User id set
-	 * @throws AppException
-	 */
-	public List<Integer> getIds() throws AppException {
-		// Initialiaze ids
-		List<Integer> ids = new ArrayList<Integer>();
-		
-		//Declare Connection object,PreparedStatement object and ResultSet object
-		Connection conn = null;
-		PreparedStatement psmt = null;
-		ResultSet rs = null;
-		
-		try {
-			// Create database connection
-			conn = DBUtil.getConnection();
-			// Declare operation statement:query user id set,"?" is a placeholder
-			String sql = "select id from t_user where del = 0";
-			
-			psmt = conn.prepareStatement(sql);
-			
-			rs = psmt.executeQuery();// Return result set
-			// Loop to get information in result set,and save in ids
-			while (rs.next()) {
-				ids.add(rs.getInt("id"));
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-			throw new AppException("dao.impl.UserDaoImpl.getIds");
-		} finally {
-			// Close database operation object, release resources
-			DBUtil.closeResultSet(rs);
-			DBUtil.closeStatement(psmt);
-			DBUtil.closeConnection(conn);
-		}
-		
-		return ids;
 	}
 
 }
